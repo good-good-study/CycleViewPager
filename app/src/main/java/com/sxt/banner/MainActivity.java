9db01d75dff774e5c9dd52cf5c6aa408eff60bd8
izhaohu <15617819527m@sina.cn>
2017-10-20 11:18:36 +0800

    nothing

--- app/src/main/java/com/sxt/banner/MainActivity.java
+++ app/src/main/java/com/sxt/banner/MainActivity.java
@@ -1,7 +1,9 @@
 package com.sxt.banner;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sxt.banner.library.CycleViewPager;
 
 public class MainActivity extends AppCompatActivity {
 
