adb77ea73b701ce8ec0703222d93672e287a9ec0
izhaohu <15617819527m@sina.cn>
2017-11-14 11:06:33 +0800

    修改了一些样式

--- banner_library/src/main/java/com/sxt/banner/library/CycleViewPager.java
+++ banner_library/src/main/java/com/sxt/banner/library/CycleViewPager.java
@@ -10,7 +10,6 @@ import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -24,14 +23,14 @@ public class CycleViewPager extends FrameLayout {
 
     private ViewPager viewPager;
     private BaseCycleViewPagerAdapter adapter;
-    private LinearLayout pointLayout;
+    private LinearLayout pointContainer;
     private int checkedRes;
     private int unCheckedRes;
     private int currentItem = 0;
     private int marginLeft = 8;
     private final int STOP_SCROLL = 0;
     private final int START_SCROLL = 1;
-    private long duration = 2000;
+    private long duration = 3000;
     private OnPageSelectedListener onPageSelectedListener;
     private boolean isOpenSelfScroll = false;
 
@@ -82,15 +81,18 @@ public class CycleViewPager extends FrameLayout {
         lp0.height = LayoutParams.MATCH_PARENT;
         viewPager.setLayoutParams(lp0);
         //添加指示点根布局
-        pointLayout = new LinearLayout(getContext());
-        addView(pointLayout);
-        LayoutParams lp1 = (LayoutParams) pointLayout.getLayoutParams();
-        pointLayout.setPadding(0, 2, 0, 2);
+        pointContainer = new LinearLayout(getContext());
+        addView(pointContainer);
+        LayoutParams lp1 = (LayoutParams) pointContainer.getLayoutParams();
+        pointContainer.setPadding(0, 2, 0, 2);
         lp1.gravity = Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM;
         lp1.bottomMargin = 20;
         lp1.width = LayoutParams.WRAP_CONTENT;
         lp1.height = LayoutParams.WRAP_CONTENT;
-        pointLayout.setLayoutParams(lp1);
+        pointContainer.setLayoutParams(lp1);
+
+        checkedRes = R.drawable.check;
+        unCheckedRes = R.drawable.uncheck;
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -100,8 +102,6 @@ public class CycleViewPager extends FrameLayout {
             viewPager.setAdapter(adapter);
             checkPoint(currentItem);
             viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-                int currentIndex;
-
                 @Override
                 public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
@@ -109,7 +109,7 @@ public class CycleViewPager extends FrameLayout {
 
                 @Override
                 public void onPageSelected(int position) {
-                    currentIndex = position;
+                    currentItem = position;
                     if (onPageSelectedListener != null) {
                         int fixPosition;
                         if (position == 0) {
@@ -119,6 +119,7 @@ public class CycleViewPager extends FrameLayout {
                         } else {
                             fixPosition = position - 1;
                         }
+                        checkPoint(fixPosition);
                         onPageSelectedListener.onPageSelected(viewPager, adapter, fixPosition);
                     }
                 }
@@ -126,47 +127,43 @@ public class CycleViewPager extends FrameLayout {
                 @Override
                 public void onPageScrollStateChanged(int state) {
                     if (state == ViewPager.SCROLL_STATE_IDLE) {
-                        if (currentIndex == 0) {
-                            currentIndex = adapter.getCount() - 2;
-                        } else if (currentIndex == adapter.getCount() - 1) {
-                            currentIndex = 1;
+                        if (currentItem == 0) {
+                            currentItem = adapter.getCount() - 2;
+                        } else if (currentItem == adapter.getCount() - 1) {
+                            currentItem = 1;
                         }
-                        currentItem = currentIndex;
-                        viewPager.setCurrentItem(currentIndex, false);
-                        checkPoint(currentIndex);
+                        viewPager.setCurrentItem(currentItem, false);
                     }
-                }
-            });
-
-            viewPager.setOnTouchListener(new OnTouchListener() {
-                float downX = 0;
-                float moveX = 0;
-
-                @Override
-                public boolean onTouch(View view, MotionEvent event) {
-                    switch (event.getAction()) {
-                        case MotionEvent.ACTION_DOWN:
-                            downX = event.getX();
-                            break;
-                        case MotionEvent.ACTION_MOVE:
-                            moveX = event.getX();
-                            if (isOpenSelfScroll && Math.abs(moveX - downX) > 5) {
-                                stopScroll();
-                            }
-                            break;
-                        case MotionEvent.ACTION_UP:
-                            if (isOpenSelfScroll) {
-                                startScroll();
-                            }
-                            downX = 0;
-                            moveX = 0;
-                            break;
-                    }
-                    return false;
                 }
             });
         }
         return this;
+    }
+
+    float downX = 0;
+    float moveX = 0;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downX = event.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                moveX = event.getX();
+                if (isOpenSelfScroll && Math.abs(moveX - downX) > 5) {
+                    stopScroll();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (isOpenSelfScroll) {
+                    startScroll();
+                }
+                downX = 0;
+                moveX = 0;
+                break;
+        }
+        return super.onTouchEvent(event);
     }
 
     public CycleViewPager setPointRes(int checkedRes, int unCheckedRes) {
@@ -177,17 +174,13 @@ public class CycleViewPager extends FrameLayout {
 
     public CycleViewPager setCurrentItem(int currentItem) {
         this.currentItem = currentItem;
-        checkPoint(currentItem);
         if (currentItem >= 0 && viewPager != null && adapter != null && adapter.getCount() > 0)
             viewPager.setCurrentItem(currentItem % adapter.getCount());
         return this;
     }
 
     public int getCurrentItem() {
-        if (viewPager != null) {
-            return viewPager.getCurrentItem();
-        }
-        return -1;
+        return currentItem;
     }
 
     public CycleViewPager setScrollSelfState(boolean isOpenSelfScroll) {
@@ -198,18 +191,18 @@ public class CycleViewPager extends FrameLayout {
     public CycleViewPager checkPoint(int position) {
         if (viewPager == null || viewPager.getAdapter() == null || viewPager.getAdapter().getCount() == 0)
             return this;
-        pointLayout.removeAllViews();
-        if (position <= 0) position = 1;
+        pointContainer.removeAllViews();
         for (int i = 0; i < adapter.getCount() - 2; i++) {
             ImageView img = new ImageView(getContext());
-            if (position - 1 == i) {
+            if (position == i) {
                 img.setImageResource(checkedRes);
             } else {
                 img.setImageResource(unCheckedRes);
             }
-            pointLayout.addView(img);
+            pointContainer.addView(img);
             LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) img.getLayoutParams();
             lp.leftMargin = marginLeft;
+            lp.gravity = Gravity.CENTER_VERTICAL;
             img.setLayoutParams(lp);
         }
         return this;
